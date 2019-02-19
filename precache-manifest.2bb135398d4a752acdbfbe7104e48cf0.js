6fc07e97d18dc68feed55dc2e239812b30e13942
Kristoffer Tolboll <Kristofferthetolboll@gmail.com>
2019-02-19 21:13:09 +0100

    Updates

---
+++ precache-manifest.2bb135398d4a752acdbfbe7104e48cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mikro_oekonomi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d9341405025085f746d5",
+    "url": "/mikro_oekonomi/static/js/runtime~main.d9341405.js"
+  },
+  {
+    "revision": "4df72595e9a8d412f8b5",
+    "url": "/mikro_oekonomi/static/js/main.4df72595.chunk.js"
+  },
+  {
+    "revision": "1997bba6ae770ec6e29a",
+    "url": "/mikro_oekonomi/static/js/2.1997bba6.chunk.js"
+  },
+  {
+    "revision": "4df72595e9a8d412f8b5",
+    "url": "/mikro_oekonomi/static/css/main.9b385eaf.chunk.css"
+  },
+  {
+    "revision": "27cfbb85142996b35db612f37f29b453",
+    "url": "/mikro_oekonomi/index.html"
+  }
+];
